ceb89a3f2c9903bef9df895d19a7f6ca9df549bc
Shekhar Jain <shekhar@jain.la>
2020-07-21 13:01:39 -0700

    First Commit

---
+++ src/test/java/la/jain/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package la.jain.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
