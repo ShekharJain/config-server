54abf092ba0dab6c769a3f3b63dce330db7ae7cc
Shekhar Jain <shekhar@jain.la>
2020-07-21 15:45:30 -0700

    FInal Product

--- src/main/java/la/jain/configserver/ConfigServerApplication.java
+++ src/main/java/la/jain/configserver/ConfigServerApplication.java
@@ -2,7 +2,9 @@ package la.jain.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
